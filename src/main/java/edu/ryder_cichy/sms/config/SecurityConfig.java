06e426d400b6c32df9faa95e27de7b473cd0e278
Jan Cichy <04cichy@gmail.com>
2025-02-23 19:08:16 +0100

    Fixed blocked static elements

--- src/main/java/edu/ryder_cichy/sms/config/SecurityConfig.java
+++ src/main/java/edu/ryder_cichy/sms/config/SecurityConfig.java
@@ -1,25 +1,27 @@
 package edu.ryder_cichy.sms.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorizeRequests ->
-                    authorizeRequests
-                            .anyRequest().permitAll()
-//                            .requestMatchers("/login").permitAll()
-//                            .anyRequest().authenticated()
+                        authorizeRequests
+                                .requestMatchers("/login", "/**").permitAll()
+                                .anyRequest().authenticated()
                 )
-//                .httpBasic(withDefaults())
-//                .formLogin(form -> form.loginPage("/login").permitAll())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .httpBasic(httpBasic -> httpBasic.disable())
                 .build();
     }
 }
